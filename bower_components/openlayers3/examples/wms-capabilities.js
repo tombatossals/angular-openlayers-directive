710cef30fed70b6f646c49a49b222953b0c1edf7
tombatossals <david.rubert@gmail.com>
2014-09-07 15:56:08 +0200

    rework

--- bower_components/openlayers3/examples/wms-capabilities.js
+++
@@ -1,8 +0,0 @@
-goog.require('ol.format.WMSCapabilities');
-
-var parser = new ol.format.WMSCapabilities();
-
-$.ajax('data/ogcsample.xml').then(function(response) {
-  var result = parser.read(response);
-  $('#log').html(window.JSON.stringify(result, null, 2));
-});
