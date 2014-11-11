a8eea7bda1e97d8e47c2d0b20e2e6a3bebf31586
David H. Bronke <dbronke@armt.com>
2014-11-11 09:53:31 -0600

    Fixed map.removeLayer() call.

--- src/directives/layers.js
+++ src/directives/layers.js
@@ -45,7 +45,7 @@ angular.module('openlayers-directive').directive('layers', function($log, $q, ol
                             var activeLayers = map.getLayers();
                             for (var i in activeLayers) {
                                 if (activeLayers[i] === layer) {
-                                    map.removeLayer(layers);
+                                    map.removeLayer(layer);
                                 }
                             }
                             delete olLayers[name];
