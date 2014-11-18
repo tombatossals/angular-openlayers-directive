86f72c08cc81056d58fc339b3fa7a995596b561d
tombatossals <david.rubert@gmail.com>
2014-11-18 22:49:31 +0100

    label shows ok now

--- grunt/watch.js
+++ grunt/watch.js
@@ -6,7 +6,7 @@ module.exports = function (grunt, options) {
             livereload: 7777
         },
         source: {
-            files: ['src/**/*.js', 'test/unit/**.js', 'test/e2e/**.js'],
+            files: ['src/**/*.js', 'test/unit/**.js', 'test/e2e/**.js', 'css/*.css'],
             tasks: [
                 'jshint',
                 'jscs',
