133d6bc7b3e2e017bb3e083f26ac47885bf28f41
tombatossals <david.rubert@gmail.com>
2014-12-30 17:52:49 +0100

    heatmap example

---
+++ js/controllers/layer-heatmap.js
@@ -0,0 +1,36 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('LayerHeatmapController', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 43.88,
+                lon: 7.57,
+                zoom: 2
+            },
+            defaults: {
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            layers: {
+                main: {
+                    source: {
+                        type: 'OSM'
+                    }
+                },
+                heatmap: {
+                    type: 'Heatmap',
+                    source: {
+                        type: 'KML',
+                        projection: 'EPSG:3857',
+                        url: 'examples/kml/earthquakes.kml',
+                        radius: 5
+                    }
+                }
+            }
+        });
+    });
+
+})();
