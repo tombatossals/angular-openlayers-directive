f2e31bb1848683873711c8cd49acc458f5892c5f
tombatossals <david.rubert@gmail.com>
2014-12-30 15:04:26 +0100

    geojson change style example added

---
+++ js/controllers/layer-geojson-change-style.js
@@ -0,0 +1,42 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('LayerGeoJSONChangeStyleController', function($scope) {
+        angular.extend($scope, {
+            europe: {
+                lat: 39.2,
+                lon: -1.27,
+                zoom: 5
+            },
+            defaults: {
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            layers: {
+                main: {
+                    source: {
+                        type: 'OSM'
+                    }
+                },
+                esp: {
+                    source: {
+                        type: 'GeoJSON',
+                        url: 'examples/json/ESP.geo.json'
+                    },
+                    style: {
+                        fill: {
+                            color: 'red'
+                        },
+                        stroke: {
+                            color: 'white',
+                            width: 3
+                        }
+                    }
+                }
+            }
+        });
+    });
+
+})();
