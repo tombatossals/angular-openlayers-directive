b42d75aca24ecd96959d355a7ace6b08e9be73a7
tombatossals <david.rubert@gmail.com>
2014-12-31 10:59:19 +0100

    multiple layers example

---
+++ js/controllers/layer-multiple.js
@@ -0,0 +1,61 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('LayerMultipleController', function($scope) {
+        angular.extend($scope, {
+            europe: {
+                lat: 43.88,
+                lon: 7.57,
+                zoom: 3
+            },
+            defaults: {
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            layers: {
+                geographyclass: {
+                    name: 'Mapbox Geography Class',
+                    visible: false,
+                    opacity: 1,
+                    source: {
+                        type: 'TileJSON',
+                        url: 'http://api.tiles.mapbox.com/v3/mapbox.geography-class.jsonp'
+                    }
+                },
+
+                bing: {
+                    visible: true,
+                    name: 'Bing Maps',
+                    opacity: 0.8,
+                    source: {
+                        type: 'BingMaps',
+                        key: 'Aj6XtE1Q1rIvehmjn2Rh1LR2qvMGZ-8vPS9Hn3jCeUiToM77JFnf-kFRzyMELDol',
+                        imagerySet: 'Road'
+                    }
+                },
+
+                topojson: {
+                    visible: true,
+                    name: 'TopoJSON of Europe',
+                    opacity: 0.3,
+                    source: {
+                        type: 'TopoJSON',
+                        url: 'examples/json/world.topo.json'
+                    },
+                    style: {
+                        fill: {
+                            color: 'yellow'
+                        },
+                        stroke: {
+                            color: 'white',
+                            width: 3
+                        }
+                    }
+                }
+            }
+        });
+    });
+
+})();
