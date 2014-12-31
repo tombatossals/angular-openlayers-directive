181589eae4371065d2a1fd9c6a776c888f37a368
tombatossals <david.rubert@gmail.com>
2014-12-31 13:01:08 +0100

    marker example

---
+++ js/controllers/marker.js
@@ -0,0 +1,20 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('MarkerController', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 51.505,
+                lon: -0.09,
+                zoom: 8
+            },
+            defaults: {
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            }
+        });
+    });
+
+})();
