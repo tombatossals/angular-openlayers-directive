6252b87e9723095d77bc03fad407dede2671a2ed
tombatossals <david.rubert@gmail.com>
2014-12-31 14:08:40 +0100

    marker add-remove example

---
+++ js/controllers/marker-add-remove.js
@@ -0,0 +1,38 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('MarkerAddRemoveController', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 51.505,
+                lon: -0.09,
+                zoom: 7
+            },
+            defaults: {
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            markers: [
+                {
+                    name: 'London',
+                    active: true,
+                    lat: 51.505,
+                    lon: -0.09
+                }, {
+                    name: 'Bath',
+                    active: true,
+                    lat: 51.375,
+                    lon: -2.35
+                }, {
+                    name: 'Canterbury',
+                    active: false,
+                    lat: 51.267,
+                    lon: 1.083
+                }
+            ]
+        });
+    });
+
+})();
