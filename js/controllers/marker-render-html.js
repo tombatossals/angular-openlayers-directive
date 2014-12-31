26fd30cc65657bec8bae753b24330750d65a41cc
tombatossals <david.rubert@gmail.com>
2014-12-31 17:06:21 +0100

    added marker-render-html example

---
+++ js/controllers/marker-render-html.js
@@ -0,0 +1,67 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('MarkerRenderHTMLController', function($scope) {
+        var markers = [
+            {
+                name: 'Eiffel Tower',
+                lat: 48.858093,
+                lon: 2.294694,
+                label: {
+                    message: '<img src="examples/images/eiffel.jpg" />',
+                    show: false,
+                    showOnMouseOver: true
+                }
+            },
+            {
+                name: 'Notre Dame de Paris',
+                lat: 48.852966,
+                lon: 2.349902,
+                label: {
+                    message: '<img src="examples/images/notredame.jpg" />',
+                    show: true,
+                    showOnMouseOver: true
+                }
+            },
+            {
+                name: 'Palace of Versailles',
+                lat: 48.804722,
+                lon: 2.121782,
+                label: {
+                    message: '<img src="examples/images/versailles.jpg" />',
+                    show: false,
+                    showOnMouseOver: true
+                }
+            }
+        ];
+
+        angular.extend($scope, {
+            center: {
+                lat: 48.828093,
+                lon: 2.294694,
+                zoom: 11
+            },
+            defaults: {
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            markers: markers
+        });
+
+        $scope.centerAndShow = function(marker) {
+            if (!marker.label.show) {
+                markers.filter(function(marker) {
+                    marker.label.show = false;
+                });
+                $scope.center.lat = marker.lat;
+                $scope.center.lon = marker.lon;
+            }
+            marker.label.show = !marker.label.show;
+            console.log($scope.markers);
+        };
+
+    });
+
+})();
