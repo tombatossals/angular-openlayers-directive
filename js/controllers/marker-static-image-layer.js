5504b6781e1af7d4a4a36c6b82339873c17c1a6f
tombatossals <david.rubert@gmail.com>
2014-12-31 18:13:14 +0100

    markers with static image layer

---
+++ js/controllers/marker-static-image-layer.js
@@ -0,0 +1,53 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('MarkerStaticImageLayerController', function($scope) {
+        var markers = [
+            {
+                coord: [853, 670],
+                projection: 'pixel',
+                label: {
+                    message: '<img src="examples/images/eiffel.jpg" />',
+                    show: false,
+                    showOnMouseOver: true
+                }
+            },
+            {
+                coord: [980, 580],
+                projection: 'pixel',
+                label: {
+                    message: '<img src="examples/images/gizah.jpg" />',
+                    show: false,
+                    showOnMouseOver: true
+                }
+            }
+        ];
+        angular.extend($scope, {
+            center: {
+                coord: [900, 600],
+                zoom: 2
+            },
+            markers: markers,
+            defaults: {
+                view: {
+                    projection: 'pixel',
+                    extent: [0, 0, 1800, 1200]
+                },
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            layers: {
+                main: {
+                    source: {
+                        type: 'ImageStatic',
+                        url: 'http://blog.wallpops.com/wp-content/upLoads/2013/05/WPE0624.jpg',
+                        imageSize: [1800, 1200]
+                    }
+                }
+            }
+        });
+    });
+
+})();
