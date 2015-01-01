e08372d3148db3a383ecb57135fbf664a0ee202b
tombatossals <david.rubert@gmail.com>
2015-01-01 11:25:04 +0100

    multiple maps example

---
+++ js/controllers/multiple-map.js
@@ -0,0 +1,76 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('MultipleMapController', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 43.7350,
+                lon: -79.3734,
+                zoom: 10
+            },
+            defaults: {
+                view: {
+                    maxZoom: 11,
+                    minZoom: 3
+                },
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            layers1: {
+                main: {
+                    source: {
+                        type: 'OSM'
+                    }
+                },
+                geojson: {
+                    source: {
+                        type: 'GeoJSON',
+                        url: 'examples/json/toronto1.json'
+                    }
+                }
+            },
+            layers2: {
+                main: {
+                    source: {
+                        type: 'Stamen',
+                        layer: 'terrain'
+                    }
+                },
+                geojson: {
+                    source: {
+                        type: 'GeoJSON',
+                        url: 'examples/json/toronto2.json'
+                    }
+                }
+            },
+            markers1: {
+                one: {
+                    lat: 43.75,
+                    lon: -79.56
+                },
+                two: {
+                    lat: 43.76,
+                    lon: -79.50
+                }
+            },
+            markers2: {
+                one: {
+                    lat: 43.75,
+                    lon: -79.56
+                },
+                two: {
+                    lat: 43.75,
+                    lon: -79.45
+                },
+                three: {
+                    name: 'Marker Three',
+                    lat: 43.81,
+                    lon: -79.26
+                }
+            }
+        });
+    });
+
+})();
