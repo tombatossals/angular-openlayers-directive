6941faf7a5c8738b57ac8bb03128b15c8aa43033
tombatossals <david.rubert@gmail.com>
2015-01-01 15:19:56 +0100

    controls example

---
+++ js/controllers/control.js
@@ -0,0 +1,36 @@
+(function() {
+
+    var app = angular.module('webapp');
+
+    app.controller('ControlController', function($scope) {
+        angular.extend($scope, {
+            center: {
+                lat: 43.88,
+                lon: 7.57,
+                zoom: 4
+            },
+            defaults: {
+                interactions: {
+                    mouseWheelZoom: false
+                }
+            },
+            view: {
+                rotation: -1
+            },
+            controls: {
+                zoom: true,
+                fullscreen: true,
+                rotate: true
+            }
+        });
+
+        $scope.degreesToRadians = function() {
+            $scope.view.rotation = parseFloat($scope.degrees, 10).toFixed(2) * (Math.PI / 180);
+        };
+
+        $scope.$watch('view.rotation', function(value) {
+            $scope.degrees = (value * 180 / Math.PI).toFixed(2);
+        });
+    });
+
+})();
