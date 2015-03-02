fe2c5152f7cd628281578e56fdef8dfa3c661027
nbiton <naor.biton@gmail.com>
2015-03-02 14:22:39 +0800

    Doing .preventDefault only if a marker related action was taken

--- src/directives/marker.js
+++ src/directives/marker.js
@@ -72,7 +72,6 @@ angular.module('openlayers-directive')
 
                     scope.$watch('properties', function(properties) {
                         function showLabelOnEvent(evt) {
-                            evt.preventDefault();
                             if (properties.label.show) {
                                 return;
                             }
@@ -82,7 +81,9 @@ angular.module('openlayers-directive')
                                 return feature;
                             });
 
+                            var actionTaken = false;
                             if (feature === marker) {
+                                actionTaken = true;
                                 found = true;
                                 if (!isDefined(label)) {
                                     if (data.projection === 'pixel') {
@@ -98,9 +99,14 @@ angular.module('openlayers-directive')
                             }
 
                             if (!found && label) {
+                                actionTaken = true;
                                 map.removeOverlay(label);
                                 label = undefined;
                                 map.getTarget().style.cursor = '';
+                            }
+
+                            if (actionTaken) {
+                                evt.preventDefault();
                             }
                         }
 
