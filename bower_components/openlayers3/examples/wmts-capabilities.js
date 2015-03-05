aa6b4ba8efea3d80cf1a272fb2e38358abd5504f
tombatossals <david.rubert@gmail.com>
2015-03-05 18:04:16 +0100

    updated ol3

--- bower_components/openlayers3/examples/wmts-capabilities.js
+++
@@ -1,8 +0,0 @@
-goog.require('ol.format.WMTSCapabilities');
-
-var parser = new ol.format.WMTSCapabilities();
-
-$.ajax('data/WMTSCapabilities.xml').then(function(response) {
-  var result = parser.read(response);
-  $('#log').html(window.JSON.stringify(result, null, 2));
-});
