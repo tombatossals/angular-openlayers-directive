90343e7672f309f368373b7c71612248d37a3028
Juri <juri.strumpflohner@gmail.com>
2015-07-13 16:27:36 +0200

    add test task which launches tests in real browser
    this makes it a lot easier to debug the tests with karma.

--- grunt/karma.js
+++ grunt/karma.js
@@ -17,6 +17,22 @@ module.exports = function (grunt, options) {
                     'test/unit/*.js'
                 ]
             }
+        },
+        dev: {
+            frameworks: ['jasmine'],
+            browsers: ['Chrome', 'IE'],
+            singleRun: false,
+            autoWatch: true,
+            options: {
+                files: [
+                    'bower_components/openlayers3/build/ol-debug.js',
+                    'bower_components/angular/angular.js',
+                    'bower_components/angular-sanitize/angular-sanitize.js',
+                    'bower_components/angular-mocks/angular-mocks.js',
+                    'dist/angular-openlayers-directive.js',
+                    'test/unit/*.js'
+                ]
+            }
         }
     };
 };
