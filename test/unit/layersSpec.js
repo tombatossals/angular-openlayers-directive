tombatossals/angular-openlayers-directive

commit 5d6d5e73c7150cc7a26b5fd69b6d73258b465cb4

fix(layers): The layer style must be preserved if only changed the source. Thanks to @mfdev1 for reporting here: https://github.com/tombatossals/angular-openlayers-directive/issues/127

src/directives/layer.js
@@ -101,8 +101,7 @@ angular.module('openlayers-directive').directive('olLayer', function($log, $q, o
 
                     } else {
 
-                        if (isDefined(oldProperties) &&
-                           !(equals(properties, oldProperties) && equals(properties.style, oldProperties.style))) {
+                        if (isDefined(oldProperties) && !equals(properties.source, oldProperties.source)) {
 
                             if (!equals(properties.source, oldProperties.source)) {
                                 var idx = olLayer.index;
@@ -115,6 +114,15 @@ angular.module('openlayers-directive').directive('olLayer', function($log, $q, o
                                         setVectorLayerEvents(defaults.events, map, scope, properties.name);
                                     }
                                 }
+                            }
+
+                            if (properties.style) {
+                                if (!angular.isFunction(properties.style)) {
+                                    style = createStyle(properties.style);
+                                } else {
+                                    style = properties.style;
+                                }
+                                olLayer.setStyle(style);
                             }
 
                             if (isDefined(properties.index) && properties.index !== olLayer.index) {
@@ -131,15 +139,15 @@ angular.module('openlayers-directive').directive('olLayer', function($log, $q, o
                                     olLayer.setOpacity(properties.opacity);
                                 }
                             }
+                        }
 
-                            if (isDefined(properties.style) && !equals(properties.style, oldProperties.style)) {
-                                if (!angular.isFunction(properties.style)) {
-                                    style = createStyle(properties.style);
-                                } else {
-                                    style = properties.style;
-                                }
-                                olLayer.setStyle(style);
+                        if (isDefined(properties.style) && !equals(properties.style, oldProperties.style)) {
+                            if (!angular.isFunction(properties.style)) {
+                                style = createStyle(properties.style);
+                            } else {
+                                style = properties.style;
                             }
+                            olLayer.setStyle(style);
                         }
                     }
                 }, true);

examples/055-layers-geojon-dynamic-load-example.html
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+  <!DOCTYPE html>
 <html ng-app="demoapp">
   <head>
     <script src="../bower_components/openlayers3/build/ol.js"></script>
@@ -12,7 +12,7 @@
 
         $http.get('json/ITA.geo.json').success(function(data) {
             var italy = data;
-            $scope.ita = {
+            $scope.layer = {
                 source: {
                     type: 'GeoJSON',
                     geojson: {
@@ -32,23 +32,39 @@
             }
         });
 
+        var sourceTemplate =  {
+            type: 'GeoJSON',
+            geojson: {
+                projection: 'EPSG:3857',
+                object: {}
+            }
+        };
+
+        $scope.update = function () {
+            $http.get("json/FRA.geo.json").success(function (data) {
+                $scope.layer.source = angular.copy(sourceTemplate);
+                $scope.layer.source.geojson.object = data;
+            });
+        };
+
         angular.extend($scope, {
             europe: {
                 lat: 43.88,
                 lon: 7.57,
                 zoom: 5
             },
-            ita: {}
+            layer: {}
         });
       } ]);
       </script>
   </head>
   <body ng-controller="GeoJSONController">
      <openlayers ol-center="europe" height="400px">
-         <ol-layer ol-layer-properties="ita"></ol-layer>
+         <ol-layer ol-layer-properties="layer"></ol-layer>
      </openlayers>
      <h1>Layers inline GeoJSON example</h1>
+     <button ng-click="update()">Load France GeoJSON</button>
      <p>You can load inline GeoJSON as new layer easily, look at the source of the <strong>layers</strong> attribute:</p>
-     <pre ng-bind="ita | json"></pre>
+     <pre ng-bind="layer | json"></pre>
   </body>
 </html>

commit 1d9225dd48157fe77042100e97add2df438d13f9

fix(layers): layer properties should update when being set
Due to #127 , commit 5d6d5e73c7150cc7a26b5 introduced a bug s.t.
updating the layer's visibility property did not have any effect as it did
not enter the if-clause where the visibility was updated.

src/directives/layer.js
@@ -100,48 +100,56 @@ angular.module('openlayers-directive').directive('olLayer', function($log, $q, o
                         }
 
                     } else {
+                        var isNewLayer = (function(olLayer) {
+                            // this function can be used to verify whether a new layer instance has
+                            // been created. This is needed in order to re-assign styles, opacity
+                            // etc...
+                            return function(layer) {
+                                return layer !== olLayer;
+                            };
+                        })(olLayer);
 
+                        // set source properties
                         if (isDefined(oldProperties) && !equals(properties.source, oldProperties.source)) {
+                            var idx = olLayer.index;
+                            layerCollection.removeAt(idx);
 
-                            if (!equals(properties.source, oldProperties.source)) {
-                                var idx = olLayer.index;
-                                layerCollection.removeAt(idx);
-                                olLayer = createLayer(properties, projection);
-                                if (isDefined(olLayer)) {
-                                    insertLayer(layerCollection, idx, olLayer);
+                            olLayer = createLayer(properties, projection);
 
-                                    if (detectLayerType(properties) === 'Vector') {
-                                        setVectorLayerEvents(defaults.events, map, scope, properties.name);
-                                    }
-                                }
-                            }
+                            if (isDefined(olLayer)) {
+                                insertLayer(layerCollection, idx, olLayer);
 
-                            if (properties.style) {
-                                if (!angular.isFunction(properties.style)) {
-                                    style = createStyle(properties.style);
-                                } else {
-                                    style = properties.style;
-                                }
-                                olLayer.setStyle(style);
-                            }
-
-                            if (isDefined(properties.index) && properties.index !== olLayer.index) {
-                                removeLayer(layerCollection, olLayer.index);
-                                insertLayer(layerCollection, properties.index, olLayer);
-                            }
-
-                            if (isBoolean(properties.visible) && properties.visible !== oldProperties.visible) {
-                                olLayer.setVisible(properties.visible);
-                            }
-
-                            if (properties.opacity !== oldProperties.opacity) {
-                                if (isNumber(properties.opacity) || isNumber(parseFloat(properties.opacity))) {
-                                    olLayer.setOpacity(properties.opacity);
+                                if (detectLayerType(properties) === 'Vector') {
+                                    setVectorLayerEvents(defaults.events, map, scope, properties.name);
                                 }
                             }
                         }
 
-                        if (isDefined(properties.style) && !equals(properties.style, oldProperties.style)) {
+                        // set opacity
+                        if (isDefined(oldProperties) &&
+                            properties.opacity !== oldProperties.opacity || isNewLayer(olLayer)) {
+                            if (isNumber(properties.opacity) || isNumber(parseFloat(properties.opacity))) {
+                                olLayer.setOpacity(properties.opacity);
+                            }
+                        }
+
+                        // set index
+                        if (isDefined(properties.index) &&
+                            properties.index !== olLayer.index || isNewLayer(olLayer)) {
+                            removeLayer(layerCollection, olLayer.index);
+                            insertLayer(layerCollection, properties.index, olLayer);
+                        }
+
+                        // set visibility
+                        if (isDefined(oldProperties) &&
+                            isBoolean(properties.visible) &&
+                            properties.visible !== oldProperties.visible || isNewLayer(olLayer)) {
+                            olLayer.setVisible(properties.visible);
+                        }
+
+                        // set style
+                        if (isDefined(properties.style) &&
+                            !equals(properties.style, oldProperties.style) || isNewLayer(olLayer)) {
                             if (!angular.isFunction(properties.style)) {
                                 style = createStyle(properties.style);
                             } else {

test/unit/layersSpec.js
@@ -97,4 +97,104 @@ describe('Directive: openlayers layers', function() {
         scope.$digest();
         expect(olLayers.getLength()).toBe(0);
     });
+
+    it('should update the visibility of a layer', function() {
+        var layers = [
+            {
+                name: 'mapbox',
+                source: {
+                    type: 'TileJSON',
+                    url: 'http://api.tiles.mapbox.com/v3/mapbox.geography-class.jsonp'
+                },
+                visible: true
+            }
+        ];
+        angular.extend(scope, { layers: layers });
+        var element = angular.element('<openlayers custom-layers="true">' +
+                                        '<ol-layer name="{{ layer.name }}" ol-layer-properties="layer" ' +
+                                                'ng-repeat="layer in layers">',
+                                      '</openlayers>');
+        $compile(element)(scope);
+        scope.$digest();
+
+        // act
+        layers[0].visible = false;
+
+        // assert
+        var mapboxLayer;
+        olData.getMap().then(function(olMap) {
+            var olLayers = olMap.getLayers();
+            olLayers.forEach(function(layer) {
+                if (layer.get('name') === 'mapbox') {
+                    mapboxLayer = layer;
+                }
+            });
+        });
+
+        scope.$digest();
+        expect(mapboxLayer).toBeDefined();
+        expect(mapboxLayer.getVisible()).toBeFalsy();
+    });
+
+    it('should keep the style changes across layer updates', function() {
+        var layers = [
+            {
+                name: 'mapbox',
+                source: {
+                    type: 'GeoJSON',
+                    url: 'http://api.tiles.mapbox.com/v3/mapbox.geography-class.json'
+                },
+                style: {
+                    fill: {
+                        color: 'rgba(255, 0, 255, 0.6)'
+                    },
+                    stroke: {
+                        color: 'white',
+                        width: 3
+                    }
+                }
+            }
+        ];
+        angular.extend(scope, { layers: layers });
+        var element = angular
+                        .element('<openlayers custom-layers="true">' +
+                                    '<ol-layer name="{{ layer.name }}" ol-layer-properties="layer" ' +
+                                        'ng-repeat="layer in layers">',
+                                  '</openlayers>');
+        $compile(element)(scope);
+        scope.$digest();
+
+        var style;
+        olData.getMap().then(function(olMap) {
+            var olLayers = olMap.getLayers();
+            olLayers.forEach(function(layer) {
+                if (layer.get('name') === 'mapbox') {
+                    style = layer.getStyle();
+                }
+            });
+        });
+
+        scope.$digest();
+        expect(style).toBeDefined();
+
+        // act
+        layers[0].source.url = 'http://api.tiles.mapbox.com/v3/mapbox.satellite-class.json';
+        scope.$digest();
+
+        // assert
+        var styleAfter;
+        olData.getMap().then(function(olMap) {
+            var olLayers = olMap.getLayers();
+            olLayers.forEach(function(layer) {
+                if (layer.get('name') === 'mapbox') {
+                    styleAfter = layer.getStyle();
+                }
+            });
+        });
+
+        scope.$digest();
+        expect(styleAfter).toBeDefined();
+        expect(style).toEqual(styleAfter);
+    });
+
 });