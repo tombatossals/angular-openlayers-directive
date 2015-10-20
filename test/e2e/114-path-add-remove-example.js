7b88392e30970994a7d9a7680f349887d9b4320b
hubjac1 <hubjac1@gmail.com>
2015-10-20 13:01:47 +0200

    test: e2e test for path-add-remove-example
    Test scenario for path-add-remove-example
    
    Test failed: Loading 114-path-add-remove-example.html remove path should clear path on map

---
+++ test/e2e/114-path-add-remove-example.js
@@ -0,0 +1,62 @@
+'use strict';
+describe('Loading 114-path-add-remove-example.html', function() {
+
+  var expectedPathList = [
+      {
+         message: "This is a path",
+         coords: "[[[-58.38,-34.6],[-58.58,-34.6]]]"
+      },
+      {
+         message: "And this is another path",
+         coords: "[[[-58.38,-34.7],[-58.38,-34.8]]]"
+      },
+      {
+         message: "Hello there",
+         coords: "[[[-58.48,-34.4],[-58.58,-34.5]]]"
+      }
+    ];
+
+    beforeAll(function() {
+        browser.get('/examples/114-path-add-remove-example.html');
+    }, 30000);
+
+    it('add should display label on map', function() {
+        var displayPath = element.all(by.className('popup-label'));
+        
+        var displayLabels = displayPath.map(function(elem){
+        	return elem.getAttribute('message');
+        });
+
+        var expectedLabels = expectedPathList.map(function(elem){
+          return elem.message;
+        });
+
+        expect(displayLabels).toEqual(expectedLabels.reverse());
+    });
+
+    it('add should display path on map', function() {
+        var displayPath = element.all(by.className('path'));
+        
+        var displayCoords = displayPath.map(function(elem){
+          return elem.getAttribute('coords');
+        });
+
+        var expectedCoordinate = expectedPathList.map(function(elem){
+          return elem.coords;
+        });
+
+        expect(displayCoords).toEqual(expectedCoordinate.reverse());
+    });
+
+    it('remove should clear path on map', function() {
+
+        var removeButton = element(by.id('add-remove-btn'));
+        removeButton.click();
+        browser.waitForAngular();
+
+        var displayPath = element.all(by.className('popup-label'));
+
+        expect(displayPath.count()).toBe(0);
+    });
+
+});
