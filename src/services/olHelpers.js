491e8edbf7e043c3c40b242f6527e07cc3123686
mfdev1 <mfdev@incrementp.co.jp>
2016-06-30 00:00:58 +0900

    feat(olHelpers): custom imageExtent support for ImageStatic layers (#279)

--- src/services/olHelpers.js
+++ src/services/olHelpers.js
@@ -538,7 +538,7 @@ angular.module('openlayers-directive').factory('olHelpers', function($q, $log, $
                     attributions: createAttribution(source),
                     imageSize: source.imageSize,
                     projection: projection,
-                    imageExtent: projection.getExtent(),
+                    imageExtent: source.imageExtent ? source.imageExtent : projection.getExtent(),
                     imageLoadFunction: source.imageLoadFunction
                 });
                 break;
