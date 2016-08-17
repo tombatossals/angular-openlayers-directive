f54d08d41a922a74aa1f6cf6e1a704557545a554
Clay Anderson <supersonicclay@gmail.com>
2016-08-17 16:12:34 -0600

    Change to openlayers-prebuilt

--- grunt/concat.js
+++ grunt/concat.js
@@ -3,7 +3,7 @@
 var banner = '(function (root, factory) {\n' +
              '    if (typeof require === \'function\' && typeof exports === \'object\') {\n' +
              '        // CommonJS\n' +
-             '        var ol = require(\'openlayers\');\n' +
+             '        var ol = require(\'openlayers-prebuilt\');\n' +
              '        var ngSanitize = require(\'angular-sanitize\');\n' +
              '        exports.angularOpenlayersDirective = factory(ol);\n' +
              '    } else if (typeof define === \'function\' && define.amd) {\n' +
